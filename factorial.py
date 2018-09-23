097e5938a6197ecb9e55ebe42b398b3c544575f1
Eziowrf <eziocheng@gmail.com>
2018-09-23 16:23:27 -0400

    This is a test of update

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
 # Zhihang Cheng
-def factorial(a):
-    while a>0:
-        a*=(a-1)
-        a-=1
+def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
 
